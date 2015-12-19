11c1d841ca9c3ac7b6aaa60a0b8d53307a95b823
acoross <acoross@gmail.com>
2015-12-20 00:09:32 +0900

    upload

---
+++ FileNamesChanger/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace FileNamesChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btSearch = new System.Windows.Forms.Button();
+            this.tbNewName = new System.Windows.Forms.TextBox();
+            this.btStringTrans = new System.Windows.Forms.Button();
+            this.btRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(574, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "열기...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(552, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(13, 57);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(551, 184);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 267);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(552, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // btSearch
+            // 
+            this.btSearch.Location = new System.Drawing.Point(655, 267);
+            this.btSearch.Name = "btSearch";
+            this.btSearch.Size = new System.Drawing.Size(75, 23);
+            this.btSearch.TabIndex = 4;
+            this.btSearch.Text = "검색";
+            this.btSearch.UseVisualStyleBackColor = true;
+            this.btSearch.Click += new System.EventHandler(this.btSearch_Click);
+            // 
+            // tbNewName
+            // 
+            this.tbNewName.Location = new System.Drawing.Point(12, 294);
+            this.tbNewName.Name = "tbNewName";
+            this.tbNewName.Size = new System.Drawing.Size(552, 21);
+            this.tbNewName.TabIndex = 7;
+            // 
+            // btStringTrans
+            // 
+            this.btStringTrans.Location = new System.Drawing.Point(574, 267);
+            this.btStringTrans.Name = "btStringTrans";
+            this.btStringTrans.Size = new System.Drawing.Size(75, 23);
+            this.btStringTrans.TabIndex = 8;
+            this.btStringTrans.Text = "구문변환";
+            this.btStringTrans.UseVisualStyleBackColor = true;
+            this.btStringTrans.Click += new System.EventHandler(this.btStringTrans_Click);
+            // 
+            // btRun
+            // 
+            this.btRun.Location = new System.Drawing.Point(574, 296);
+            this.btRun.Name = "btRun";
+            this.btRun.Size = new System.Drawing.Size(75, 23);
+            this.btRun.TabIndex = 9;
+            this.btRun.Text = "실행";
+            this.btRun.UseVisualStyleBackColor = true;
+            this.btRun.Click += new System.EventHandler(this.btRun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 390);
+            this.Controls.Add(this.btRun);
+            this.Controls.Add(this.btStringTrans);
+            this.Controls.Add(this.tbNewName);
+            this.Controls.Add(this.btSearch);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "파일명 일괄 변환기";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btSearch;
+        private System.Windows.Forms.TextBox tbNewName;
+        private System.Windows.Forms.Button btStringTrans;
+        private System.Windows.Forms.Button btRun;
+    }
+}
+
---
+++ FileNamesChanger/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+/* 숫자를 *로 해서 검색하는 것은 성공. 
+ * TODO: 길이가 정해지지 않은 임의의 문자열 지정 기능 추가.
+ */
+namespace FileNamesChanger
+{
+    public partial class Form1 : Form
+    {
+        string      path;
+        string frontPattern, backPattern;                
+
+        public Form1()
+        {
+            InitializeComponent();
+            path = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            if (openFileDialog1.FileNames.Length  > 0)
+            {
+                path = openFileDialog1.FileName.TrimEnd(openFileDialog1.SafeFileName.ToCharArray());
+                textBox1.Text = path;
+
+                textBox2.Text = openFileDialog1.SafeFileName;
+
+                System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(path);
+                System.IO.FileInfo[] fis = di.GetFiles();
+
+                string[] srcFilenames = new string[fis.Length];
+                int i = 0;
+                foreach (System.IO.FileInfo fi in fis)
+                {
+                    srcFilenames[i++] = fi.Name;
+                }
+                listBox1.DataSource = srcFilenames;
+            }            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex < 0 || listBox1.SelectedIndex > listBox1.Items.Count)
+                return;
+
+            textBox2.Text = (string)listBox1.Items[listBox1.SelectedIndex];
+        }
+
+        private string PatternStringToREX(string srcPattern)
+        {
+            char[] escapeChars = { '[', ']', '+', '.', '(', ')'};
+
+            foreach (char ch in escapeChars)
+            {
+                srcPattern = InsertEscapeToString(srcPattern, ch);
+            }
+
+            return srcPattern;
+        }
+
+        private string InsertEscapeToString(string src, char ch)
+        {
+            int idx1 = 0;
+            int idx = src.IndexOf(ch, idx1);
+            while (idx != -1)
+            {
+                src = src.Insert(idx, @"\");
+                idx1 = idx + 2;
+                idx = src.IndexOf(ch, idx1);
+            }
+
+            return src;
+        }
+
+        /* 방법: 숫자위치를 *로  표시.  검색-> 파일목록 가져오고,  각각을 번호와함께 저장.
+         * 변환문자열 *과 함께 기록. (*은 숫자 위치)
+         * '변환' 버튼을 누르면 변환됨. */
+        private void btSearch_Click(object sender, EventArgs e)        
+        {
+            SearchAndBind();    
+        }
+
+        void SearchAndBind()
+        {
+            string pattern = StringToREX2(textBox2.Text);
+
+            if (pattern.TrimEnd("\0".ToArray<char>()) == "")
+            {
+                return;
+            }
+
+            if (path == "")
+            {
+                return;
+            }
+
+            SearchResult sr = FindMatchingList(pattern, path);
+
+            if (sr != null)
+                listBox1.DataSource = sr.filenames;    
+        }
+
+        SearchResult FindMatchingList(string aPattern, string aPath)
+        {
+            System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(aPath);
+            System.IO.FileInfo[] fis = di.GetFiles();
+
+            string[] filenames = new string[fis.Length];            
+            string[] numbers = new string[fis.Length];
+
+            int i = 0;
+            try
+            {
+                foreach (System.IO.FileInfo fi in fis)
+                {
+                    System.Text.RegularExpressions.Regex regex = new System.Text.RegularExpressions.Regex(aPattern);
+                    Match match = regex.Match(fi.Name);
+
+                    if (match.Success)
+                    {                        
+                        filenames[i] = match.Captures[0].Value;
+                        numbers[i] = match.Groups[1].Value;
+                        ++i;
+                    }
+                }
+
+                SearchResult sr = new SearchResult(filenames, numbers, null, i);
+
+                return sr;
+            }
+            catch
+            {
+                Console.WriteLine("Error!!!");
+
+                return null;
+            }
+        }
+        
+        private bool PatternStringDiscriminate(string src)
+        {
+            int idx = src.IndexOf('*');
+
+            if (src.IndexOf('*', idx + 1) > -1)
+            {
+                return false;
+            }
+
+            if (idx != -1)
+            {
+                frontPattern = src.Substring(0, idx);
+                backPattern = src.Substring(idx + 1);
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+                
+        private string StringToREX2(string src)
+        {
+            src =  PatternStringToREX(src);
+
+            if (src.Contains(@"***"))
+            {
+                src = src.Replace(@"***", @"([0-9]{3})");
+            }
+            else if (src.Contains(@"**"))
+            {
+                src = src.Replace(@"**", @"([0-9]{2})");
+            }
+            else if (src.Contains(@"*"))
+            {
+                src = src.Replace(@"*", @"([0-9])");
+            }
+            else
+            {
+                return src;
+            }
+
+            while (src.Contains(@"$"))
+            {
+                src = src.Replace(@"$", @"(.+)");
+            }
+
+            return src;
+        }
+
+        private void btStringTrans_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = StringToREX2(textBox2.Text);
+        }
+
+        private void btRun_Click(object sender, EventArgs e)
+        {
+            if (tbNewName.Text.Trim() == "")
+            {
+                return;
+            }
+
+            string pattern = StringToREX2(textBox2.Text);
+
+            if (pattern.TrimEnd("\0".ToArray<char>()) == "")
+            {
+                return;
+            }
+
+            if (path == "")
+            {
+                return;
+            }
+
+            SearchResult sr =  FindMatchingList(pattern, path);
+
+            string[] newnames = new string[sr.found];
+                       
+            for (int i = 0; i < sr.found; ++i)
+            {
+                newnames[i] = GetNewName(sr.nums[i]);
+                if(newnames[i] == "null")
+                {
+                    MessageBox.Show("Dest file name error.");
+
+                    return;
+                }                
+            }
+
+            for (int i = 0; i < sr.found; ++i)
+            {
+                System.IO.File.Move(path + sr.filenames[i], path + newnames[i]);
+            }
+
+            SearchAndBind();
+        }
+
+        private string GetNewName(string num)
+        {
+            if (tbNewName.Text.Trim() == "")
+            {
+                return "null";
+            }
+
+            string tplt = tbNewName.Text;
+
+            if (tplt.Contains(@"***"))
+            {
+                tplt = tplt.Replace(@"***", num);
+            }
+            else if (tplt.Contains(@"**"))
+            {
+                tplt = tplt.Replace(@"**", num);
+            }
+            else if (tplt.Contains(@"*"))
+            {
+                tplt = tplt.Replace(@"*", num);
+            }
+            else
+            {
+                return "null";
+            }
+
+            return tplt;
+        }
+    }
+
+    public class SearchResult
+    {
+        public string[] filenames;
+        public string[] nums;
+        public string[] var;
+        public int found;
+
+        private SearchResult()
+        {
+        }
+
+        public SearchResult(string[] fn, string[] n, string[] v, int f)
+        {
+            if (fn != null)
+                filenames = (string[])fn.Clone();
+
+            if (n != null)
+                nums = (string[])n.Clone();
+
+            if (v != null)
+                var = (string[])v.Clone();
+
+            found = f;
+        }
+    }
+
+    
+}
